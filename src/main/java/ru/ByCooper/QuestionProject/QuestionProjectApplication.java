fa69c4aa6072bdcfa07bc448750fc6cd73f3b9b0
ByCooper <kup.official@yahoo.com>
2023-08-14 15:48:54 +0300

    Курсовая работа/обычная сложность(скелет)

---
+++ src/main/java/ru/ByCooper/QuestionProject/QuestionProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.ByCooper.QuestionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ByCooper/QuestionProject/QuestionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ByCooper.QuestionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
